7a64812322b9418ea18231b99043bb76e31df349
bella-le <le.bella98@gmail.com>
2020-02-13 14:59:16 -0800

    View spaces reads from json data

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ import Popularity from './popularity';
 import StudySpaceCard from './study-space-card';
 import SurveyForm from './survey-form';
 import Survey from './survey';
-import Profile from './ttt-profile';
+import Profile from './profile';
 import Topbar from './topbar';
 
 export { Ambience, Homebar, Popularity, StudySpaceCard, SurveyForm, Survey, Profile, Topbar };
