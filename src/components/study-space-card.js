1818e0193af2f3518e77af07579222914e3d36eb
axpecial <and043@ucsd.edu>
2020-02-13 11:53:43 -0800

    Adjusted the image dimensions, image cropping, font size, and card height of the study space card. The card should be flexible enough to adapt to multiple device dimensions.

--- src/components/study-space-card.js
+++ src/components/study-space-card.js
@@ -1,33 +1,46 @@
 import React from "react";
 import styled from "styled-components";
-import {Card} from 'react-bootstrap';
+import {
+  Container,
+  Card
+} from 'react-bootstrap';
 
 /**
  * Props Expected:
  *  - title: name of the study space
  *  - description: brief description of the study space
  *  - imageFilePath: file path to locally stored image of the study space
- *
+ */
+// TODO: Link the card to the corresponding detail page.
+const StudySpaceCard = (props) => (
+  <a style={{ color: 'inherit' }} href="">
+    <Card style={{ height: '10rem', width: '100%', flexDirection: 'row', overflow: 'scroll' }}>
+      <div style={{ height: '100%', width: '40%', overflow: 'hidden' }}>
+        <Card.Img style={{ objectFit: 'cover', height: '10rem' }} variant="top" src={require("../assets/study-spaces/galbraith.jpg")} />
+      </div>
+      <Card.Body>
+        <Card.Title style={{ fontSize: '100%' }}>{props.title}</Card.Title>
+        <Card.Text style={{ fontSize: '60%' }}>
+          {props.description}
+        </Card.Text>
+        <Card.Text style={{ fontSize: '75%' }}>
+          {props.distance}
+        </Card.Text>
+        <Card.Text style={{ fontSize: '60%' }}>
+          Tags: {props.tags}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </a>
+);
+/*
  * Sources of Help:
  *  - Card: https://react-bootstrap.github.io/components/cards/#basic-example
  *  - Card.Img orientation: https://css-tricks.com/snippets/css/a-guide-to-flexbox/
- */
-const StudySpaceCard = (props) => (
-  <Card style={{ width: '100%', flexDirection: 'row' }}>
-    <Card.Img style={{ width: '250px' }} variant="top" src={require("../assets/study-spaces/galbraith.jpg")} />
-    <Card.Body>
-      <Card.Title>{props.title}</Card.Title>
-      <Card.Text>
-        {props.description}
-      </Card.Text>
-      <Card.Text>
-        {props.distance}
-      </Card.Text>
-      <Card.Text>
-        Tags: {props.tags}
-      </Card.Text>
-    </Card.Body>
-  </Card>
-);
+ *  - Card.Img cropping:
+ *      https://react-bootstrap.github.io/layout/grid/#container-props,
+ *      https://alligator.io/css/cropping-images-object-fit/
+ *  - Card clickable: https://stackoverflow.com/questions/49554070/using-bootstrap-cards-as-a-hyperlink
+ **/
 
 export default StudySpaceCard;
