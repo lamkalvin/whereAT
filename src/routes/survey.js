56f2e352998bc889e4f7277cf19cf58b13149433
bella ✨ <le.bella98@gmail.com>
2020-02-16 16:45:49 -0800

    Changes to Router, View spaces, Survey (#15)

---
+++ src/routes/survey.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import SurveyForm from '../components/survey-form';
+import Topbar from '../components/topbar';
+
+const BufferDiv = styled.div`
+    margin: 50px auto 50px auto;
+    width: 85%;
+`;
+
+const Survey = (props) => {
+    const data = props.data;
+
+    return (
+        <div>
+            <Topbar title='Survey' hasBack handleClick={props.handleClick} />
+            <BufferDiv>
+                <h5>Study room</h5>
+                <p>{data.title}</p>
+                <SurveyForm data={data}
+                    handleClick={props.handleClick}
+                    handleSubmit={props.handleSubmit} />
+            </BufferDiv>
+        </div>
+    );
+};
+
+export default Survey;
