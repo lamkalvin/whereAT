fd1aea582006022949ae5be8f9d9835fe3cdc54c
lamkalvin <32418155+lamkalvin@users.noreply.github.com>
2020-02-18 19:38:43 -0800

    W7 add study spaces (#22)
    * Fixed AddForm to properly upload new data to localStorage and navigate to view-item page for new data; Updated app-container to pass both json data and local storage data; fixed presentational logic in ambience and popularity to only have 3 options and display for newly created spaces
    
    * Reverted merge-conflict changes; fixed bug where data was not being updated with customSpace data from localStorage
    
    * Resolved merge changes with week 7

--- src/routes/homePage/HomePage.js
+++ src/routes/homePage/HomePage.js
@@ -9,6 +9,7 @@ import { Link, withRouter } from "react-router-dom";
 const pencilIcon = require('../../assets/icons/pencil.png');
 const pinIcon = require('../../assets/icons/pin.png');
 const stackIcon = require('../../assets/icons/stack.png');
+const addIcon = require('../../assets/icons/bookmark-add.png');
 
 const TileButton = styled(Button)`
     display: flex;
@@ -25,13 +26,18 @@ const TileButton = styled(Button)`
     padding-left: 20px;
 `
 
+const BufferDiv = styled.div`
+    margin: 50px auto 100px auto;
+    width: 85%;
+`;
+
 const HomePage = (props) => {
     let titles = props.data.map(({ title }) => title)
 
     return (
         <div>
             <Topbar title='where @' />
-            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', margin: '20px' }}>
+            <BufferDiv style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
                 <Autocomplete suggestions={titles} data={props.data} history={props.history} />
 
 
@@ -39,26 +45,30 @@ const HomePage = (props) => {
                     <TileButton type="submit">
                         <Image src={pencilIcon} style={{ paddingRight: '20px' }} />
                         Specify preferences
-                </TileButton>
+                    </TileButton>
                 </Link>
 
                 <Link to="/location-search">
                     <TileButton type="submit">
                         <Image src={pinIcon} style={{ paddingRight: '20px' }} />
                         Search by location
-                </TileButton>
+                    </TileButton>
                 </Link>
 
                 <Link to="/recently-viewed">
                     <TileButton type="submit">
                         <Image src={stackIcon} style={{ paddingRight: '20px' }} />
                         Recently viewed
-                </TileButton>
+                    </TileButton>
                 </Link>
-            </div>
-            <div>
 
-            </div>
+                <Link to="/new-space">
+                    <TileButton type="submit">
+                        <Image src={addIcon} style={{ paddingRight: '20px' }}/>
+                        Add study space
+                    </TileButton>
+                </Link>
+            </BufferDiv>
         </div>
     );
 }
